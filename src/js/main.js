e1e0e7d1fbfd21be4999403b0ae5bef001a1c93a
Dv_Andrew <dvoeglazov.andrey24@gmail.com>
2018-08-12 19:37:03 +0700

    Научился рисовать стрелочку, из которой потом можно запилить корабль

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,10 @@
 import { drawGrid } from './modules/learnCanvas.min.js';
 import { drawPackman } from './modules/learnCanvas.min.js';
+import { drawShip } from './modules/learnCanvas.min.js';
 
 var canvas = document.querySelector('.game__asteroids');
 var context = canvas.getContext('2d');
 
 drawGrid(canvas, context);
 
-drawPackman(canvas, context, 200, 200, 100, Math.random());
+drawShip(canvas, context, 200, 200, 150, {guide: true});
--- src/js/modules/learnCanvas.js
+++ src/js/modules/learnCanvas.js
@@ -1,57 +1,96 @@
-export function drawGrid(canvas, context, minor, major, stroke, fill) {
+export function drawGrid(cvs, ctx, minor, major, stroke, fill) {
     minor = minor || 10;
     major = major || minor * 5;
-    stroke = stroke || "#00ff00";
-    fill = fill || "#009900";
+    stroke = stroke || '#00ff00';
+    fill = fill || '#009900';
 
-    context.save();
+    ctx.save();
 
-    context.strokeStyle = stroke;
-    context.fillStyle = fill;
+    ctx.strokeStyle = stroke;
+    ctx.fillStyle = fill;
 
-    let width = canvas.width;
-    let height = canvas.height;
+    let width = cvs.width;
+    let height = cvs.height;
 
     for(var x = 0; x < width; x+=minor) {
-        context.beginPath();
-        context.moveTo(x, 0);
-        context.lineTo(x, height);
-        context.lineWidth = (x % major == 0) ? 0.5 : 0.25;
-        context.stroke();
+        ctx.beginPath();
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, height);
+        ctx.lineWidth = (x % major == 0) ? 0.5 : 0.25;
+        ctx.stroke();
         if (x % major == 0) {
-            context.fillText(x, x + 2, 10);
+            ctx.fillText(x, x + 2, 10);
         }
     }
     for(var y = 0; y < height; y+=minor) {
-        context.beginPath();
-        context.moveTo(0, y);
-        context.lineTo(width, y);
-        context.lineWidth = (y % major == 0) ? 0.5 : 0.25;
-        context.stroke();
+        ctx.beginPath();
+        ctx.moveTo(0, y);
+        ctx.lineTo(width, y);
+        ctx.lineWidth = (y % major == 0) ? 0.5 : 0.25;
+        ctx.stroke();
         if (y % major == 0) {
             if (y != 0) {
-                context.fillText(y, 2, y + 10);
+                ctx.fillText(y, 2, y + 10);
             }
         }
     }
 
-    context.restore();
+    ctx.restore();
 }
 
-export function drawPackman(canvas, context, x, y, radius, mouth) {
-    x = x || canvas.width / 2;
-    y = y || canvas.height / 2;
+export function drawPackman(cvs, ctx, x, y, radius, mouth) {
+    x = x || cvs.width / 2;
+    y = y || cvs.height / 2;
     radius = radius || 100;
     mouth = mouth || 0;
 
-    context.save();
+    ctx.save();
 
-    context.beginPath();
-    context.fillStyle = 'yellow';
-    context.arc(x, y, radius, Math.PI * (0 + (mouth / 4)), Math.PI * (2 - (mouth / 4)));
-    context.lineTo(x, y);
-    context.closePath();
-    context.fill();
+    ctx.beginPath();
+    ctx.fillStyle = 'yellow';
+    ctx.arc(x, y, radius, Math.PI * (0 + (mouth / 4)), Math.PI * (2 - (mouth / 4)));
+    ctx.lineTo(x, y);
+    ctx.closePath();
+    ctx.fill();
 
-    context.restore();
+    ctx.restore();
+}
+
+export function drawShip(cvs, ctx, x, y, radius, options) {
+    options = options || {};
+
+    ctx.save();
+
+    ctx.lineWidth = options.lineWidth || 2;
+    ctx.strokeStyle = options.strokeStyle || 'white';
+    ctx.fillStyle = options.fillStyle || 'black';
+
+    let angle = (options.angle || Math.PI * 0.5) / 2;
+
+    ctx.beginPath();
+    ctx.moveTo(x + radius, y);
+    ctx.lineTo(
+        x + Math.cos(Math.PI - angle) * radius,
+        y + Math.sin(Math.PI - angle) * radius
+    );
+    ctx.lineTo(
+        x + Math.cos(Math.PI + angle) * radius,
+        y + Math.sin(Math.PI + angle) * radius
+    );
+
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+
+    if(options.guide) {
+        ctx.strokeStyle = 'red';
+        ctx.fillStyle = 'rgba(255, 150, 0, 0.15)';
+        ctx.lineWidth = 0.5;
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, Math.PI * 2);
+        ctx.stroke();
+        ctx.fill();
+    }
+
+    ctx.restore();
 }
